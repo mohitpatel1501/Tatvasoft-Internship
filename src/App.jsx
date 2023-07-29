b0b52a8b352ccad9cd2bd0634cadb106541f0b41
mohitpatel1501 <mohitgovinda2001@gmail.com>
2023-07-29 12:27:21 +0530

    Day-2 Routing, Creating components and props

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./Pages/Home";
+import Book from "./Pages/Book";
+import Login from "./Pages/Login";
+
+
+import Navbar from "./Components/global/Navbar";
+import Footer from "./Components/global/Footer";
 
 const App = () => {
   return (
-    <div>Book Sell Project</div>
-  )
-}
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/book" element={<Book />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
